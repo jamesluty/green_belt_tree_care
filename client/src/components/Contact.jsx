955eb861ba6ca55f8f4a76a1c476cbc3e2620075
James Luty <luty0125@gmail.com>
2022-08-21 19:15:39 -0700

    css changes to form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,12 +1,21 @@
-import React, {useRef} from 'react'
-import tree_logo from '../imgs/tree_logo.jpg'
-import emailjs from '@emailjs/browser'
+import React, {useRef, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import tree_logo from '../imgs/tree_logo.jpg';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-    const publicKey = "QRSJh2P5Gcmyk3Gst"
-    const templateId = "template_zm3gvfh"
-    const serviceId = "service_1n9bmqw"
+    const {submitted, setSubmitted} = useState(false);
+    const navigate = useNavigate();
     const form = useRef();
+
+    // Email JS Information
+    const publicKey = "QRSJh2P5Gcmyk3Gst";
+    const templateId = "template_zm3gvfh";
+    const serviceId = "service_1n9bmqw";
+
+    const goHome = () => {
+        navigate("/");
+    }
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -17,29 +26,48 @@ const Contact = () => {
             }, (error) => {
                 console.log(error.text);
             })
+
+        navigate("/");
     }
+
     return (
         <div>
             <div className="navbar">
                 <img className="logo" src={tree_logo} alt="logo"/>
                 <h1 className="companyName">Green Belt Tree Care LLC</h1>
+                <button className="homeBtn" onClick={goHome}>Home</button>
             </div>
-            <div className="contactHeader">
+            <div>
                 <h1 className="contactTitle">Contact Us</h1>
-                <p>Fill out the form below to request a quote and we will get back to you as soon as possible. You can also give us a call at <span>206-531-6525</span>.</p>
-            </div>
-            <div className="requestFormDiv">
+                <p className="contactSubTitle">Fill out the form below to request a quote and we will get back to you as soon as possible. <br/>You can also give us a call at <b>206-531-6525</b>.</p>
+
+                <div className="requestFormDiv">
                     <form className="requestForm" ref={form} onSubmit={sendEmail}>
-                        <label>Full Name:</label>
-                        <input type="text" name="from_name"/>
-                        <label>Email:</label>
-                        <input type="text" name="email"/>
-                        <label>Phone Number:</label>
-                        <input type="text" name="phone_number"/>
-                        <label>Message to send:</label>
-                        <input type="text" name="message"/>
+                        <div className="formName">
+                            <div className="nameRow">
+                                <label>First Name:</label>
+                                <input className="nameInput" type="text" name="first_name"/>
+                            </div>
+                            <div className="nameRow">
+                                <label>Last Name:</label>
+                                <input className="nameInput" type="text" name="last_name"/>
+                            </div>
+                        </div>
+                        <div className="formInput">
+                            <label>Email:</label>
+                            <input type="text" name="email"/>
+                        </div>
+                        <div className="formInput">
+                            <label>Phone Number:</label>
+                            <input type="text" name="phone_number"/>
+                        </div>
+                        <div className="formInput">
+                            <label>Message:</label>
+                            <input type="text" name="message"/>
+                        </div>
                         <input type="submit" value="send"/>
                     </form>
+                </div>
             </div>
         </div>
     )
