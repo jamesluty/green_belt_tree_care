4b1aea3ca93997b534092dd4be6b25f0fe68926d
James Luty <luty0125@gmail.com>
2022-09-24 08:38:01 -0700

    added background image to divs and more content

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,8 @@ import wyatt from '../imgs/wyatt.jpg';
 import tree_before from '../imgs/tree_before.jpg';
 // import tree_after from '../imgs/tree_after.jpg';
 import forest from '../imgs/forest.jpg';
-import tree from '../imgs/tree.jpeg'
+import tree_trimming from '../imgs/tree_trimming.jpg';
+import tree_background from '../imgs/tree_background.png';
 
 const Home = () => {
 
@@ -21,15 +22,20 @@ const Home = () => {
                 {/* Image Carousel */}
                 <div style={{ backgroundImage: `url(${forest})` }} className="bannerDiv">
                     <h1 className="bannerHeader">Need help with taking care of your trees?</h1>
-                    <h2 className="bannerSubText">With over 10 years of experience we are here to help take care of your tree care needs.</h2>
+                    <h2 className="bannerSubText">With over 10 years of experience we are here to help you with of your tree care needs.</h2>
                     <p className="bannerDetails">We provide a range of services such trimming, pruning, tree removal, stump grinding and more.</p>
                     {/* <h1 className="slogan">We'll go out on a limb for you.</h1> */}
+                </div>
+
+                {/* Contact section */}
+                <div className="contactDiv">
+                    <p className="contactInfo">To request a free quote please contact us at 206-531-6525<br/> or <Link to="/contact" className="contactMsg">send us a messge.</Link></p>
                 </div>
 
                 {/* About section */}
                 <div className="aboutDiv">
                     <div className="about">
-                        <div className="aboutDetails">
+                        <div style={{ backgroundImage: `url(${tree_background})`}} className="aboutDetails">
                             <h1 className="aboutTitle">About Us</h1>
                             <p className="aboutMsg">Green Belt Tree Care is a Native American owned and operated tree care company based out of Auburn, WA. We are Licensed, Insured and Bonded. We are committed to providing top quality tree care services throughout western washington. We offer a range of services from tree trimming, tree removal, stump grinding and many more. We also offer 24 hour emergency services.</p>
                         </div>
@@ -40,56 +46,58 @@ const Home = () => {
                 {/* Tree Maintenance */}
                 <div className="maintenanceDiv">
                     <div className="maintenance">
-                        <h1 className="maintenanceTitle">Why is Tree Maintenance so Important?</h1>
-                        <p className="maintenanceDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <img className="trimmingImg" src={tree_trimming} alt="image_unavailable" />
+                        <div style={{ backgroundImage: `url(${tree_background})`}} className="maintenanceDetails">
+                            <h1 className="maintenanceTitle">Why is Tree Maintenance so Important?</h1>
+                            <p className="maintenanceDesc">There are many benefits to properly maintaining but here are the 5 main reasons why proper tree maintenance is so important.</p>
+                            <ol className="maintenanceList">
+                                <li>Preventative tree maintenance will improve the overall structure of your trees, keeping them from developing weak branching habits.</li>
+                                <li>Trimming prevents limbs from growing weak crotches or eventually crossing each other and competing for space.</li>
+                                <li>Removing dead or dying limbs reduces the stree on the tree by elimating fuel for burrowing insects, fungus and bacteria.</li>
+                                <li>If dead branches break off and fall they can potentially cause damage to property or injury to someone.</li>
+                                <li>A trimmed tree is more aethetically pleasing and can improve the appearance of your property and increase it's value.</li>
+                            </ol>
+                        </div>
                     </div>
                 </div>
-
-                {/* Contact section */}
-                <div className="contactDiv">
-                    <p className="contactInfo">To request a free quote please contact us at 206-531-6525 or <Link to="/contact" className="contactMsg">send us a messge.</Link></p>
-                </div>
+                <hr  className="diffSep"/>
 
                 {/* Service section */}
-                <div className="servicesDiv">
-                    <h1 className="servicesTitle">See the difference we can make</h1>
-                    <div className="serviceTypes">
-                        <div className="service">
-                            <h2 className="servTitle">Tree Trimming</h2>
-                            {/* <div className="imgSlt">
-                                <h2 onClick={before} id="before">Before</h2>
-                                <h2 onClick={after} id="after">After</h2>
-                            </div> */}
-                            <img src={tree_before} alt="image_not_available" className="serviceImg"/>
-                        </div>
-                        <div className="service">
-                            <h2 className="servTitle">Tree Removal</h2>
-                            {/* <div className="imgSlt">
-                                <h2 onClick={before} id="before">Before</h2>
-                                <h2 onClick={after} id="after">After</h2>
-                            </div> */}
-                            <img src={tree_before} alt="image_not_available" className="serviceImg"/>
-                        </div>
-                    </div>
-                    <div className="serviceTypes">
-                        <div className="service">
-                            <h2 className="servTitle">Stump Grinding</h2>
-                            {/* <div className="imgSlt">
-                                <h2 onClick={before} id="before">Before</h2>
-                                <h2 onClick={after} id="after">After</h2>
-                            </div> */}
-                            <img src={tree_before} alt="image_not_available" className="serviceImg"/>
-                        </div>
-                        <div className="service">
-                            <h2 className="servTitle">Tree Pruning</h2>
-                                {/* <div className="imgSlt">
-                                <h2 onClick={before} id="before">Before</h2>
-                                <h2 onClick={after} id="after">After</h2>
-                            </div> */}
-                            <img src={tree_before} alt="image_not_available" className="serviceImg"/>
+                <div className="differenceDiv">
+                    <div className="differences">
+                        <h1 className="differenceTitle">See the difference we can make</h1>
+                        <div className="differenceSection">
+                            <div className="differenceType">
+                                <img src={tree_before} alt="image_not_available" className="imageBefore"/>
+                                <div style={{ backgroundImage: `url(${tree_background})`}} className="differenceDetails">
+                                    <h2 className="servTitle">Tree Trimming</h2>
+                                    <p>asdfasdfsss</p>
+                                </div>
+                            </div>
+                            <div className="differenceType">
+                                <div style={{ backgroundImage: `url(${tree_background})`}} className="differenceDetails">
+                                    <h2 className="servTitle">Tree Removal</h2>
+                                    <p>asdfsadfasf</p>
+                                </div>
+                                <img src={tree_before} alt="image_not_available" className="imageBefore"/>
+                            </div>
+                            <div className="differenceType">
+                                <img src={tree_before} alt="image_not_available" className="imageBefore"/>
+                                <div style={{ backgroundImage: `url(${tree_background})`}} className="differenceDetails">
+                                    <h2 className="servTitle">Stump Grinding</h2>
+                                    <p>asfasdf</p>
+                                </div>
+                            </div>
+                            <div className="differenceType">
+                                <div style={{ backgroundImage: `url(${tree_background})`}} className="differenceDetails">
+                                    <h2 className="servTitle">Tree Pruning</h2>
+                                    <p>asdfasdfsss</p>
+                                </div>
+                                <img src={tree_before} alt="image_not_available" className="imageBefore"/>
+                            </div>
+                            <Link to="/services" className="servicesLink">Click to learn more about what services we provide</Link>
                         </div>
                     </div>
-                    <Link to="/services" className="servicesLink">Click to learn more about what services we provide</Link>
                 </div>
             </div>
         </div>
