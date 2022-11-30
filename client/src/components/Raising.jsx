aab67d2be34495aadf95fa79c20a97f0964d4260
James Luty <luty0125@gmail.com>
2022-11-29 16:52:26 -0800

    created page layout for crown raising

--- client/src/components/Raising.jsx
+++ client/src/components/Raising.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Services from './Services';
 
 const Raising = () => {
     return (
-        <div>Raising</div>
+        <div>
+            {/* Sticky button link to contact page */}
+            <Link to="/contact" className="contactLink"><h3 className="contactBtn">Contact Us</h3></Link>
+
+            <div>
+                <h1 className="serviceTitle">Professional Crown Raising Service</h1>
+
+                <div className="serviceSection">
+                    <div className="serviceLeft">
+                        <h2>What is crown raising?</h2>
+                    </div>
+                    <div className="serviceRight">
+                        <Services />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
